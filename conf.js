a7948f98a3cfa893c7b384ddabf2678e074c90e1
Amit Grover <aadvikgrover@gmail.com>
2017-09-05 05:54:35 -0400

    Update conf.js

--- conf.js
+++ conf.js
@@ -10,7 +10,6 @@ exports.config = {
   specs: ['test/*_spec.js' ],
   onPrepare: function () {
          browser.driver.manage().window().setSize(1680, 1050);
-        // browser.get("http://www.istockphoto.com");
   },
   jasmineNodeOpts: {
     showColors: true,
